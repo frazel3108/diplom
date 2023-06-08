7b6ea37ab48794d0585c5c3f6d4bf6e6df83784b
Андрей Тарасов <tarad10@mail.ru>
2023-06-08 23:37:39 +0300

    dev

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -22,45 +22,6 @@ class MainController extends Controller
         $categories = $categoryRepo->mainBlocksCategoryWithProduct();
         $offers = OfferResource::collection($offerRepo->bannersMain());
 
-        // $data = [
-        //     [
-        //         'action' => 'book',
-        //         'fio' => 'Хворостьянова, С.В.  / С.В. Хворостьянова // . - 2011. - № 1. - С. 68-73. Требования к содержанию библиотечных веб-сайтов.',
-        //         'name' => 'Веб-сайт: требования к информационной структуре и наполнению',
-        //         'izda' => 'Современная библиотека',
-        //         'numizd' => '',
-        //         'year' => '2011',
-        //         'countP' => '112',
-        //     ], [
-        //         'action' => 'link',
-        //         'title' => 'Главные тренды веб-разработки и технологий на 2023',
-        //         'name' => 'Merehead',
-        //         'link' => 'https://merehead.com/ru/blog/web-tech-technology-trends-2023/',
-        //         'date' => '15.01.2023',
-        //     ]
-        // ];
-
-        // foreach ($data as $link) {
-        //     if ($link['action'] == 'book') {
-        //         $link = $link['fio'] . ' ' . $link['name']. '.'
-        //             . (!empty($link['numizd'])
-        //                 ? ' - ' . $link['numizd'] . ' изд.'
-        //                 : ''
-        //             ) . ' - ' . $link['izda'] . ', ' . $link['year'] . '.'
-        //             . (!empty($link['countP'])
-        //                 ? ' - ' . $link['countP'] . ' с.'
-        //                 : ''
-        //             );
-        //     } elseif ($link['action'] == 'link') {
-        //         $link = $link['title'] . ' // ' . $link['name']
-        //             . ': [сайт]. — URL: ' . $link['link']
-        //             . ' (дата обращения: ' . $link['date'] . ').';
-        //     }
-
-        //     dump($link);
-        // }
-        // die();
-
         return view(
             'modules.main',
             compact(
